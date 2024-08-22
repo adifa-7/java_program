249eaa408bf12e11560e73f42670959514815d5b
Adifa saniya <153834311+adifa-7@users.noreply.github.com>
2024-08-22 19:35:20 +0530

    Add files via upload

---
+++ swap_num_variable.java
@@ -0,0 +1,17 @@
+
+public class swap_num_variable {
+
+	public static void main(String[] args) {
+		System.out.println("Numbers after Swapping:");
+		int a = 5;
+		int b = 7;
+		int c;
+		c = a;
+		a = b;
+		b = c;
+		
+		System.out.println(a);
+		System.out.println(b);
+	}
+
+}
